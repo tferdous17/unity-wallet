f3064fab0e81e462b4d672f41a7b39f25d38ed23
Tasnim Ferdous <tferdous611@gmail.com>
2023-09-15 13:03:46 -0400

    add semantic Spring annotations

--- src/main/java/dev/tferdous/familycashcardapp/controller/CashCardController.java
+++ src/main/java/dev/tferdous/familycashcardapp/controller/CashCardController.java
@@ -1,4 +1,7 @@
 package dev.tferdous.familycashcardapp.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CashCardController {
 }
--- src/main/java/dev/tferdous/familycashcardapp/repository/CashCardRepository.java
+++ src/main/java/dev/tferdous/familycashcardapp/repository/CashCardRepository.java
@@ -2,6 +2,8 @@ package dev.tferdous.familycashcardapp.repository;
 
 import dev.tferdous.familycashcardapp.cashcard.CashCard;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CashCardRepository extends JpaRepository<Long, CashCard> {
 }
