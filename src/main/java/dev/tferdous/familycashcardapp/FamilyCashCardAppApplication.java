a02a07d8da439e6d566bdaceea0cf3f5e0369723
Tasnim Ferdous <tferdous611@gmail.com>
2023-09-15 12:52:04 -0400

    initial commit

---
+++ src/main/java/dev/tferdous/familycashcardapp/FamilyCashCardAppApplication.java
@@ -0,0 +1,13 @@
+package dev.tferdous.familycashcardapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyCashCardAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyCashCardAppApplication.class, args);
+    }
+
+}
