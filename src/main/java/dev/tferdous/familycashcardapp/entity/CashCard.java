ffdf908ce68e03a93d4e02c348612be87bc4f3fd
Tasnim Ferdous <tferdous611@gmail.com>
2023-09-16 19:29:09 -0400

    add SequenceGenerator and GeneratedValue annotations for auto incrementing ID column in db

--- src/main/java/dev/tferdous/familycashcardapp/entity/CashCard.java
+++ src/main/java/dev/tferdous/familycashcardapp/entity/CashCard.java
@@ -1,7 +1,6 @@
 package dev.tferdous.familycashcardapp.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +9,15 @@ import lombok.Setter;
 @Entity
 public class CashCard {
     @Id
+    @SequenceGenerator(
+            name = "card_sequence",
+            sequenceName = "card_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "card_sequence"
+    )
     private Long id;
     private Double amount;
     private String owner;
