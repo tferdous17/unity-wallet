f4bc9822bb2a92f68185bbb6321a71fcc085e131
Tasnim Ferdous <tferdous611@gmail.com>
2023-09-16 17:58:08 -0400

    change CashCard from a record to regular class

--- src/main/java/dev/tferdous/familycashcardapp/cashcard/CashCard.java
+++ src/main/java/dev/tferdous/familycashcardapp/cashcard/CashCard.java
@@ -1,4 +1,15 @@
 package dev.tferdous.familycashcardapp.cashcard;
 
-public record CashCard(Long id, Double amount, String owner) {
+public class CashCard {
+    private Long id;
+    private Double amount;
+    private String owner;
+
+    public CashCard() {}
+
+    public CashCard(Double amount, String owner) {
+        this.amount = amount;
+        this.owner = owner;
+    }
+
 }
