ea39eadd5aefac5c0a6021c7f4d44896bc61cf6b
Tasnim Ferdous <tferdous611@gmail.com>
2023-09-16 18:22:11 -0400

    add CashCardRepository object and autowired constructor

--- src/main/java/dev/tferdous/familycashcardapp/service/CashCardService.java
+++ src/main/java/dev/tferdous/familycashcardapp/service/CashCardService.java
@@ -1,4 +1,15 @@
 package dev.tferdous.familycashcardapp.service;
 
+import dev.tferdous.familycashcardapp.repository.CashCardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CashCardService {
+    private final CashCardRepository repository;
+
+    @Autowired
+    public CashCardService(CashCardRepository repository) {
+        this.repository = repository;
+    }
 }
