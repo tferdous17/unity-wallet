720a614d135e02068e97345c666ee45524b326b1
Tasnim Ferdous <tferdous611@gmail.com>
2023-09-16 19:19:05 -0400

    fix: swap input params for JpaRepository<T, ID>
    More specifically I accidentally put the ID param (Long) in place of T and the T param (CashCard) in place of ID which caused BeanCreationException

--- src/main/java/dev/tferdous/familycashcardapp/repository/CashCardRepository.java
+++ src/main/java/dev/tferdous/familycashcardapp/repository/CashCardRepository.java
@@ -1,9 +1,9 @@
 package dev.tferdous.familycashcardapp.repository;
 
-import dev.tferdous.familycashcardapp.cashcard.CashCard;
+import dev.tferdous.familycashcardapp.entity.CashCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CashCardRepository extends JpaRepository<Long, CashCard> {
+public interface CashCardRepository extends JpaRepository<CashCard, Long> {
 }
