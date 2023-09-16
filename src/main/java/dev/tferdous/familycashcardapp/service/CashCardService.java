e2c0ca796395f412ba811520386ea7b76f0d1cca
Tasnim Ferdous <tferdous611@gmail.com>
2023-09-16 19:49:58 -0400

    create getCashCards method that retrieves cards from repository

--- src/main/java/dev/tferdous/familycashcardapp/service/CashCardService.java
+++ src/main/java/dev/tferdous/familycashcardapp/service/CashCardService.java
@@ -1,8 +1,11 @@
 package dev.tferdous.familycashcardapp.service;
 
+import dev.tferdous.familycashcardapp.entity.CashCard;
 import dev.tferdous.familycashcardapp.repository.CashCardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CashCardService {
@@ -11,5 +14,9 @@ public class CashCardService {
     @Autowired
     public CashCardService(CashCardRepository repository) {
         this.repository = repository;
+    }
+
+    public List<CashCard> getCashCards() {
+        return repository.findAll();
     }
 }
