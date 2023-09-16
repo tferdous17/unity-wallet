7835a85e9e8b6944faf9940c74b74040a65cec1d
Tasnim Ferdous <tferdous611@gmail.com>
2023-09-16 18:24:00 -0400

    add CashCardService object and autowired constructor

--- src/main/java/dev/tferdous/familycashcardapp/controller/CashCardController.java
+++ src/main/java/dev/tferdous/familycashcardapp/controller/CashCardController.java
@@ -1,7 +1,15 @@
 package dev.tferdous.familycashcardapp.controller;
 
+import dev.tferdous.familycashcardapp.service.CashCardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CashCardController {
+    private CashCardService cashCardService;
+
+    @Autowired
+    public CashCardController(CashCardService cashCardService) {
+        this.cashCardService = cashCardService;
+    }
 }
