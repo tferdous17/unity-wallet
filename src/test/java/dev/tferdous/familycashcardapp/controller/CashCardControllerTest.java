42343e74b1f4647e364461319cbf7e3e0729f55a
Tasnim Ferdous <tferdous611@gmail.com>
2023-09-17 11:38:41 -0400

    remove unused imports

--- src/test/java/dev/tferdous/familycashcardapp/controller/CashCardControllerTest.java
+++ src/test/java/dev/tferdous/familycashcardapp/controller/CashCardControllerTest.java
@@ -1,7 +1,5 @@
 package dev.tferdous.familycashcardapp.controller;
 
-import dev.tferdous.familycashcardapp.repository.CashCardRepository;
-import dev.tferdous.familycashcardapp.service.CashCardService;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
