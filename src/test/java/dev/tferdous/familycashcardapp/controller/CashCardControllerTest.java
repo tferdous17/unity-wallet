cddf1ee7530f72fccb85f19d84c1bc914035ab7b
Tasnim Ferdous <tferdous611@gmail.com>
2023-09-16 20:23:55 -0400

    create test class for controller

---
+++ src/test/java/dev/tferdous/familycashcardapp/controller/CashCardControllerTest.java
@@ -0,0 +1,15 @@
+package dev.tferdous.familycashcardapp.controller;
+
+import dev.tferdous.familycashcardapp.repository.CashCardRepository;
+import dev.tferdous.familycashcardapp.service.CashCardService;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashCardControllerTest {
+
+    @Test
+    void getCashCardsReturnsAList() {
+    }
+
+}
