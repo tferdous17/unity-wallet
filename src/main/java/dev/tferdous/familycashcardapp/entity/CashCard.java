005552d7ba27617f0464c20bbc967837caa5d092
Tasnim Ferdous <tferdous611@gmail.com>
2023-09-17 12:07:03 -0400

    add Column annotations + params to id, amount, and owner

--- src/main/java/dev/tferdous/familycashcardapp/entity/CashCard.java
+++ src/main/java/dev/tferdous/familycashcardapp/entity/CashCard.java
@@ -9,17 +9,15 @@ import lombok.Setter;
 @Entity
 public class CashCard {
     @Id
-    @SequenceGenerator(
-            name = "card_sequence",
-            sequenceName = "card_sequence",
-            allocationSize = 1
-    )
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "card_sequence"
-    )
+    @SequenceGenerator(name = "card_sequence", sequenceName = "card_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "card_sequence")
+    @Column(nullable = false, unique = true, updatable = false)
     private Long id;
+
+    @Column(nullable = false)
     private Double amount;
+
+    @Column(columnDefinition = "String", nullable = false)
     private String owner;
 
     public CashCard() {}
