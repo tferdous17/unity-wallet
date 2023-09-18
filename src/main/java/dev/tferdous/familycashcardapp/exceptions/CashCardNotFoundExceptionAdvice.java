09eca92374da3eed20da28cf4cc95a56c3158cbf
Tasnim Ferdous <tferdous611@gmail.com>
2023-09-18 18:24:20 -0400

    create handler class method for CashCardNotFoundException
    any CashCardNotFoundException will get relayed to this class to be handled and return error code 404 + error message

---
+++ src/main/java/dev/tferdous/familycashcardapp/exceptions/CashCardNotFoundExceptionAdvice.java
@@ -0,0 +1,23 @@
+package dev.tferdous.familycashcardapp.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class CashCardNotFoundExceptionAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(CashCardNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> exceptionHandler(CashCardNotFoundException exception) {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("errorMessage", exception.getMessage());
+        return errorMap;
+    }
+}
