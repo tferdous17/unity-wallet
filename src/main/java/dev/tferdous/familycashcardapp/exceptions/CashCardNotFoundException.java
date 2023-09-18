56277a5f40477c9f9de26452cb1aaabd180345b5
Tasnim Ferdous <tferdous611@gmail.com>
2023-09-18 18:22:49 -0400

    typo fix

--- src/main/java/dev/tferdous/familycashcardapp/exceptions/CashCardNotFoundException.java
+++ src/main/java/dev/tferdous/familycashcardapp/exceptions/CashCardNotFoundException.java
@@ -2,6 +2,6 @@ package dev.tferdous.familycashcardapp.exceptions;
 
 public class CashCardNotFoundException extends RuntimeException {
     public CashCardNotFoundException(Long id) {
-        super("Could not find cash card with id = " + id);
+        super("Could not find existing cash card with id = " + id);
     }
 }
