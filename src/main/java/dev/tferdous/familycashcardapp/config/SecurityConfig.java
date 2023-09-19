0a2e08a57bdea9a63170c20b966b3ad686aa1acc
Tasnim Ferdous <tferdous611@gmail.com>
2023-09-19 19:56:39 -0400

    implement passwordEncoder function that returns a BCryptPasswordEncoder

--- src/main/java/dev/tferdous/familycashcardapp/config/SecurityConfig.java
+++ src/main/java/dev/tferdous/familycashcardapp/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -28,4 +30,10 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .build();
      }
+
+     @Bean
+     public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+     }
+
 }
