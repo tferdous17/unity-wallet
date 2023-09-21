17be0176a0ed2e410bf4ada99a06ee5b067a9544
Tasnim Ferdous <tferdous611@gmail.com>
2023-09-20 20:35:32 -0400

    simplify filter chain method for testing purposes

--- src/main/java/dev/tferdous/familycashcardapp/config/SecurityConfig.java
+++ src/main/java/dev/tferdous/familycashcardapp/config/SecurityConfig.java
@@ -6,8 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -19,21 +23,24 @@ public class SecurityConfig {
 
     @Bean
      public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers(HttpMethod.GET, this.baseUrl + "/**").permitAll()
-                            .requestMatchers(HttpMethod.POST, this.baseUrl).hasAuthority("ROLE_admin")
-                            .requestMatchers(HttpMethod.PUT, this.baseUrl + "/**").hasAuthority("ROLE_admin")
-                            .requestMatchers(HttpMethod.DELETE, this.baseUrl + "/**").hasAuthority("ROLE_admin")
-                            .anyRequest().authenticated();
-                })
-                .csrf(AbstractHttpConfigurer::disable)
+        return http.authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/cashcards/**")
+                            .authenticated();
+                }).csrf(c -> c.disable())
                 .build();
-     }
+//                .authorizeHttpRequests(auth -> {
+//                    auth.requestMatchers(HttpMethod.GET, this.baseUrl + "/**").permitAll()
+//                            .requestMatchers(HttpMethod.POST, this.baseUrl).hasAuthority("ROLE_admin")
+//                            .requestMatchers(HttpMethod.PUT, this.baseUrl + "/**").hasAuthority("ROLE_admin")
+//                            .requestMatchers(HttpMethod.DELETE, this.baseUrl + "/**").hasAuthority("ROLE_admin")
+//                            .anyRequest().authenticated();
+//                })
+//                .csrf(AbstractHttpConfigurer::disable)
+    }
 
      @Bean
      public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(12);
+        return new BCryptPasswordEncoder();
      }
 
 }
