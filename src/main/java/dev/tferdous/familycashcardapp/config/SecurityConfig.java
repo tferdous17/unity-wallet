453c2dae50cb9e4fa5ede44437dcd4438e15f941
Tasnim Ferdous <tferdous611@gmail.com>
2023-09-24 10:39:00 -0400

    change role auth for GET method to allow card owners to retrieve all their owned cards

--- src/main/java/dev/tferdous/familycashcardapp/config/SecurityConfig.java
+++ src/main/java/dev/tferdous/familycashcardapp/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(auth -> {
-            auth.requestMatchers(HttpMethod.GET, this.baseUrl).hasRole("ADMIN")
+            auth.requestMatchers(HttpMethod.GET, this.baseUrl).hasRole("CARD-OWNER")
                     .requestMatchers(HttpMethod.GET, this.baseUrl + "/**").hasRole("CARD-OWNER")
                     .requestMatchers(HttpMethod.POST, this.baseUrl).hasRole("CARD-OWNER")
                     .requestMatchers(HttpMethod.PUT, this.baseUrl + "/**").hasRole("ADMIN")
