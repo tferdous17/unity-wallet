c5357ab5c9ab91e48bc487d3992c71107b317093
Tasnim Ferdous <tferdous611@gmail.com>
2023-09-24 10:37:58 -0400

    add findByOwner method that returns a Page

--- src/main/java/dev/tferdous/familycashcardapp/repository/CashCardRepository.java
+++ src/main/java/dev/tferdous/familycashcardapp/repository/CashCardRepository.java
@@ -1,11 +1,14 @@
 package dev.tferdous.familycashcardapp.repository;
 
 import dev.tferdous.familycashcardapp.entity.CashCard;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CashCardRepository extends JpaRepository<CashCard, Long> {
     CashCard findByIdAndOwner(Long id, String owner);
+    Page<CashCard> findByOwner(String owner, PageRequest amount);
 
 }
