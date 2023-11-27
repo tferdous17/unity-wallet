9973c600c4e0da183e66788c6b40f078e7a01360
Tasnim Ferdous <tferdous611@gmail.com>
2023-11-27 10:02:45 -0500

    add javadoc to CashCardRepository

--- src/main/java/dev/tferdous/familycashcardapp/repository/CashCardRepository.java
+++ src/main/java/dev/tferdous/familycashcardapp/repository/CashCardRepository.java
@@ -6,9 +6,26 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository layer with custom defined find methods
+ */
 @Repository
 public interface CashCardRepository extends JpaRepository<CashCard, Long> {
+    /**
+     * This method is meant to retrieve a cash card using both the ID of the card
+     * and the name of the owner
+     * @param id ID of card
+     * @param owner Owner of card
+     * @return CashCard if found
+     */
     CashCard findByIdAndOwner(Long id, String owner);
+
+    /**
+     * This method is meant to find all the cash cards owned by any particular owner
+     * @param owner Owner of card(s)
+     * @param amount PageRequest obj
+     * @return Page of CashCard(s)
+     */
     Page<CashCard> findByOwner(String owner, PageRequest amount);
 
 }
