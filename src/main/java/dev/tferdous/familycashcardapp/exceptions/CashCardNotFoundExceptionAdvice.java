bc5c3acbc20244f1dba9ce4858b7792467d5d96f
Tasnim Ferdous <tferdous611@gmail.com>
2023-11-27 09:55:44 -0500

    add javadoc to CashCardNotFoundExceptionAdvice

--- src/main/java/dev/tferdous/familycashcardapp/exceptions/CashCardNotFoundExceptionAdvice.java
+++ src/main/java/dev/tferdous/familycashcardapp/exceptions/CashCardNotFoundExceptionAdvice.java
@@ -9,9 +9,18 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Handles a custom CashCardNotFound exception at a global level
+ * @author Tasnim Ferdous
+ */
 @ControllerAdvice
 public class CashCardNotFoundExceptionAdvice {
 
+    /**
+     * Exception handler method that stores the error message w/ exception msg in a HashMap
+     * @param exception CashCardNotFoundException
+     * @return map of error and exception msg
+     */
     @ResponseBody
     @ExceptionHandler(CashCardNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
