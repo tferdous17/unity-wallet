3040d5c46fc08a5e21f4c0ee31c15fd2269832a9
Tasnim Ferdous <tferdous611@gmail.com>
2023-11-27 09:57:30 -0500

    add javadoc to CashCardNotFoundException

--- src/main/java/dev/tferdous/familycashcardapp/exceptions/CashCardNotFoundException.java
+++ src/main/java/dev/tferdous/familycashcardapp/exceptions/CashCardNotFoundException.java
@@ -1,6 +1,14 @@
 package dev.tferdous.familycashcardapp.exceptions;
 
+/**
+ * Custom CashCardNotFound exception that's thrown when
+ * a particular CashCard is not found within the data storage
+ */
 public class CashCardNotFoundException extends RuntimeException {
+    /**
+     * Throws an error if cash card with param id not found
+     * @param id ID of cash card
+     */
     public CashCardNotFoundException(Long id) {
         super("Entity not found: Cash Card with id = " + id);
     }
