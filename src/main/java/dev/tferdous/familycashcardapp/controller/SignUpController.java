ca2a22c69e1e24d5a6cf555b469eff6cdcb62440
Tasnim Ferdous <tferdous611@gmail.com>
2024-01-07 16:59:00 -0500

    initialize controller for Sign Up page

---
+++ src/main/java/dev/tferdous/familycashcardapp/controller/SignUpController.java
@@ -0,0 +1,10 @@
+package dev.tferdous.familycashcardapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/cashcards/v1/signup")
+public class SignUpController {
+
+}
