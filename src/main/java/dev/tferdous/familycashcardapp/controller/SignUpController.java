b8000eb3340371224d1228b64c8231c4f3d313c6
Tasnim Ferdous <tferdous611@gmail.com>
2024-01-07 17:15:33 -0500

    display signup.html file when visiting api/cashcards/v1/signup

--- src/main/java/dev/tferdous/familycashcardapp/controller/SignUpController.java
+++ src/main/java/dev/tferdous/familycashcardapp/controller/SignUpController.java
@@ -1,10 +1,15 @@
 package dev.tferdous.familycashcardapp.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("api/cashcards/v1/signup")
+@Controller
 public class SignUpController {
+    @RequestMapping("api/cashcards/v1/signup")
+    public String signupTest() {
+        return "signup";
+    }
 
 }
