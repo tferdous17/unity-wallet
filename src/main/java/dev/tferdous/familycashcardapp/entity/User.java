a015f7f43db2158a1904120e9d965ee8fbcb687d
Tasnim Ferdous <tferdous611@gmail.com>
2024-01-08 12:59:52 -0500

    create User entity

---
+++ src/main/java/dev/tferdous/familycashcardapp/entity/User.java
@@ -0,0 +1,41 @@
+package dev.tferdous.familycashcardapp.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class User {
+    @Id
+    @Column(nullable = false, unique = true, updatable = false)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String firstName;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String lastName;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String username;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String email;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String password;
+
+    public User() { }
+
+    public User(String firstName, String lastName, String username, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
