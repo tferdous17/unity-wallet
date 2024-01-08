a4dcb82e540020cfef60588ef52c6bc6ec820e83
Tasnim Ferdous <tferdous611@gmail.com>
2024-01-08 13:53:43 -0500

    create UserMapper class to map User objects into UserDTOs

---
+++ src/main/java/dev/tferdous/familycashcardapp/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package dev.tferdous.familycashcardapp.mapper;
+
+import dev.tferdous.familycashcardapp.dto.UserDTO;
+import dev.tferdous.familycashcardapp.entity.User;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class UserMapper {
+
+    public UserDTO toDTO(User user) {
+        return UserDTO.builder()
+                .id(user.getId())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .build();
+    }
+}
