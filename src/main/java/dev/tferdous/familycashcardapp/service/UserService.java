57b7fd47d37531dddc33f752ddd06becfa35d744
Tasnim Ferdous <tferdous611@gmail.com>
2024-01-09 16:08:17 -0500

    set up UserService skeleton

---
+++ src/main/java/dev/tferdous/familycashcardapp/service/UserService.java
@@ -0,0 +1,16 @@
+package dev.tferdous.familycashcardapp.service;
+
+import dev.tferdous.familycashcardapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+}
