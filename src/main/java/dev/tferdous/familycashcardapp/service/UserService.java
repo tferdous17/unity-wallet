1c080474e07aa5ef3acf1daf7fcaf19fca659ae7
Tasnim Ferdous <tferdous611@gmail.com>
2024-01-09 16:11:38 -0500

    implement findUserByEmail, findUserByFullName, and findUserByUsername methods that communicate with repository layer

--- src/main/java/dev/tferdous/familycashcardapp/service/UserService.java
+++ src/main/java/dev/tferdous/familycashcardapp/service/UserService.java
@@ -1,8 +1,11 @@
 package dev.tferdous.familycashcardapp.service;
 
+import dev.tferdous.familycashcardapp.entity.User;
 import dev.tferdous.familycashcardapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -12,5 +15,17 @@ public class UserService {
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public Optional<User> findUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public Optional<User> findUserByFullName(String firstName, String lastName) {
+        return userRepository.findByFullName(firstName, lastName);
+    }
+
+    public Optional<User> findUserByUsername(String username) {
+        return userRepository.findByUsername(username);
     }
 }
