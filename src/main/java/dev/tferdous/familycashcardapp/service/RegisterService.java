03299e41b430ca033aef0d9919365dc226252fb8
Tasnim Ferdous <tferdous611@gmail.com>
2024-01-10 17:52:48 -0500

    use BCryptPasswordEncoder to encrypt user passwords before getting saved to DB

--- src/main/java/dev/tferdous/familycashcardapp/service/RegisterService.java
+++ src/main/java/dev/tferdous/familycashcardapp/service/RegisterService.java
@@ -5,16 +5,25 @@ import dev.tferdous.familycashcardapp.model.entity.User;
 import dev.tferdous.familycashcardapp.payload.request.UserRegistrationRequest;
 import dev.tferdous.familycashcardapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RegisterService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
-    public RegisterService(UserRepository userRepository) {
+    public RegisterService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     public void registerUser(UserRegistrationRequest request) {
@@ -22,7 +31,14 @@ public class RegisterService {
             throw new EmailAlreadyExistsException(request.email());
         }
 
-        User user = new User(request.firstName(), request.lastName(), request.username(), request.email(), request.password());
+        User user = new User(
+                request.firstName(),
+                request.lastName(),
+                request.username(),
+                request.email(),
+                passwordEncoder.encode(request.password())
+        );
+
         userRepository.save(user);
     }
 
