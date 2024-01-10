3083cb3bd998b8a19fdaab7a2e175067a5562536
Tasnim Ferdous <tferdous611@gmail.com>
2024-01-10 00:17:57 -0500

    refactor: findByEmail and findByUsername returns Optionals again

--- src/main/java/dev/tferdous/familycashcardapp/repository/UserRepository.java
+++ src/main/java/dev/tferdous/familycashcardapp/repository/UserRepository.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-    User findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
 }
