6216f299da51f6c3ec973d7c7be89e575c9641c9
Tasnim Ferdous <tferdous611@gmail.com>
2024-01-10 00:17:00 -0500

    implement registerUser method and use DI for RegisterService

--- src/main/java/dev/tferdous/familycashcardapp/controller/RegistrationController.java
+++ src/main/java/dev/tferdous/familycashcardapp/controller/RegistrationController.java
@@ -1,13 +1,33 @@
 package dev.tferdous.familycashcardapp.controller;
 
+import dev.tferdous.familycashcardapp.payload.request.UserRegistrationRequest;
+import dev.tferdous.familycashcardapp.service.RegisterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RegistrationController {
+
+    private final RegisterService registerService;
+
+    @Autowired
+    public RegistrationController(RegisterService registerService) {
+        this.registerService = registerService;
+    }
+
     @RequestMapping("api/cashcards/v1/register")
     public String signupTest() {
         return "registration";
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> registerUser(@RequestBody UserRegistrationRequest request) {
+        registerService.registerUser(request);
+        return ResponseEntity.ok().build();
     }
 
 }
