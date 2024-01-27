fb3508a54dd022270beafc77655a357ff23d1360
Tasnim Ferdous <tferdous611@gmail.com>
2024-01-27 14:12:47 -0500

    create UserLoginRequest

---
+++ src/main/java/dev/tferdous/familycashcardapp/payload/request/UserLoginRequest.java
@@ -0,0 +1,13 @@
+package dev.tferdous.familycashcardapp.payload.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLoginRequest {
+    private String email;
+    private String password;
+}
