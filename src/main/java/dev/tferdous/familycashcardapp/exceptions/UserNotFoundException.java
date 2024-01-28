743cfe57c7f082fd8ba20713068abc76ac9a18cc
Tasnim Ferdous <tferdous611@gmail.com>
2024-01-28 12:57:09 -0500

    add parameterized constructor for ID

--- src/main/java/dev/tferdous/familycashcardapp/exceptions/UserNotFoundException.java
+++ src/main/java/dev/tferdous/familycashcardapp/exceptions/UserNotFoundException.java
@@ -3,6 +3,10 @@ package dev.tferdous.familycashcardapp.exceptions;
 public class UserNotFoundException extends RuntimeException {
 
     public UserNotFoundException() {
-        super("User not found.");
+        super("Specified user not found.");
+    }
+
+    public UserNotFoundException(Long id) {
+        super("User with id = " + id + " was not found.");
     }
 }
